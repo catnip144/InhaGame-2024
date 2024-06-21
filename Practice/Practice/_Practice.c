16f64397fd73d388b7d0c0ba88de625680270423
catnip144 <skytower144@naver.com>
2024-06-21 16:27:31 +0900

    Template Adjustments

---
+++ Practice/Practice/_Practice.c
@@ -0,0 +1,10 @@
+﻿/*
+    
+*/
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+
+void main()
+{
+        
+}
--- Practice/PracticeSpace/PracticeSpace.c
+++
@@ -1,7 +0,0 @@
-﻿#define _CRT_SECURE_NO_WARNINGS
-#include <iostream>
-
-void main()
-{
-    
-}
