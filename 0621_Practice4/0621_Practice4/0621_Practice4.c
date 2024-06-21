c401813b2ee55f39cf6bef0cf9c074c2102a9c83
catnip144 <skytower144@naver.com>
2024-06-21 17:41:19 +0900

    06/21 Practice4 fix

--- 0621_Practice4/0621_Practice4/0621_Practice4.c
+++ 0621_Practice4/0621_Practice4/0621_Practice4.c
@@ -9,28 +9,30 @@ void main()
 	char op;
 
 	printf("사칙연산 입력(정수) : ");
-	scanf("%d%c%d", &a, &op, &b);
+
+	// %c앞에 화이트 스페이스로 공백을 주면 해당 %c 변환 문자열은 화이트 스페이스에 해당되는 문자를 받지 않는다.
+	scanf("%d %c %d", &a, &op, &b);
 
 	switch (op)
 	{
-		case '+':
-			res = a + b;
-			break;
+	case '+':
+		res = a + b;
+		break;
 
-		case '-':
-			res = a - b;
-			break;
+	case '-':
+		res = a - b;
+		break;
 
-		case '*':
-			res = a * b;
-			break;
+	case '*':
+		res = a * b;
+		break;
 
-		case '/':
-			res = a / b;
-			break;
+	case '/':
+		res = a / b;
+		break;
 
-		default:
-			break;
+	default:
+		break;
 	}
 	printf("%d%c%d=%d", a, op, b, res);
 }
