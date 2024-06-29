64c8ac8672cc9c3c402b7a8dd234414c199a5827
catnip144 <skytower144@naver.com>
2024-06-29 17:04:25 +0900

    06/29 Unity

---
+++ GameProjects/My project/Assets/Scripts/Ball.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Ω√¿€");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.Rotate(new Vector3(1, 1, 1));
+    }
+}
--- Practice/Practice/_Practice.c
+++ Practice/Practice/_Practice.c
@@ -1,10 +1,12 @@
 ﻿/*
-    
+
 */
 #define _CRT_SECURE_NO_WARNINGS
-#include <iostream>
+#include <stdio.h>
+#include <string.h>
 
 void main()
 {
-        
+
 }
+
