44f5698bfecbbe70899be1d192d560fdfd634aae
catnip144 <skytower144@naver.com>
2024-07-23 17:50:26 +0900

    07/23

---
+++ 0723_Ag/0723_Ag/0723_Ag.cpp
@@ -0,0 +1,66 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <fstream>
+#include <cmath>
+
+#define STRSIZE 50
+
+using namespace std;
+
+/*
+Q1. 비행기의 가속도 a와 이륙속도 v가 주어졌을 떄
+비행기가 이륙하기 위한 최소 활주 길이를 계산하는 프로그램을 작성하라.
+
+길이 = v^2 / 2a
+사용자에게 (m/s 단위로 v)와 (m/s^2 단위로 a)를 입력 받도록 알려주고,
+최소 활주 길이를 출력하는 프로그램을 작성
+
+ex > Speed and Acceleration : 60 3.5
+	 Minimum runway length : 514.286
+
+Q2.
+두점 (x1, y1), (x2, y2)를 입력받아 이 두 점 사이의 거리를 출력하는 프로그램을 작성
+
+거리 계산 공식 : SQUARE ROOT ( (x2 - x1)^2 + (y2 - y1)^2 )
+
+ex> x1, y1 : 1.5	-3.4
+	x2, y2 : 4		5
+
+	Distance : 8.7641
+
+Q3. c++ 기초 플러스 p368 4번
++a. 회원등록 -> 파일 저장
+b. 실명으로 열람
+...
+e. 회원이 지정한 것으로 열람
+q. 종료
+
+*/
+
+void RunwayLengthCalculator()
+{
+	double speed, acceleration;
+	cout << "Speed and Acceleration : ";
+	cin >> speed >> acceleration;
+
+	double runwayLength = pow(speed, 2) / (acceleration * 2);
+	cout << "Minimum runway length : " << runwayLength;
+}
+
+void DistanceCalculator()
+{
+	double x1, y1, x2, y2;
+	cout << "x1, y1 : ";
+	cin >> x1 >> y1;
+
+	cout << "x2, y2 : ";
+	cin >> x2 >> y2;
+
+	double distance = sqrt(pow((x2 - x1), 2) + pow((y2 - y1), 2));
+	cout << "Distance : " << distance;
+}
+
+int main()
+{
+	DistanceCalculator();
+}
---
+++ 0723_Practice2/0723_Practice2/0723_Practice2.cpp
@@ -0,0 +1,180 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <fstream>
+#include <cmath>
+#include <vector>
+#include <string>
+#include <sstream>
+
+#define FILE_PATH "../data/BopMembers.txt"
+
+using namespace std;
+/*
+Q3. c++ 기초 플러스 p368 4번
++a. 회원등록 -> 파일 저장
+b. 실명으로 열람
+...
+e. 회원이 지정한 것으로 열람
+q. 종료
+
+*/
+struct bop {
+	string fullname;
+	string title;
+	string bopname;
+	int preference;
+};
+
+enum BopId
+{
+	BOP_FULLNAME,
+	BOP_TITLE,
+	BOP_NAME,
+	BOP_PREFERENCE
+};
+
+void InitMembers(vector<bop>& members);
+void AddMember(vector<bop>& members);
+void PrintMenu();
+void PrintMembers(vector<bop>& bopList, BopId id);
+
+void main()
+{
+	vector<bop> members;
+	char option;
+
+	InitMembers(members);
+
+	while (1)
+	{
+		PrintMenu();
+		cin >> option;
+		getchar();
+		
+		switch (option)
+		{
+		case 'a':
+			AddMember(members);
+			break;
+
+		case 'b':
+			PrintMembers(members, BOP_FULLNAME);
+			break;
+
+		case 'c':
+			PrintMembers(members, BOP_TITLE);
+			break;
+
+		case 'd':
+			PrintMembers(members, BOP_NAME);
+			break;
+
+		case 'e':
+			PrintMembers(members, BOP_PREFERENCE);
+			break;
+
+		default:
+			cout << "종료합니다.";
+			return;
+		}
+	}
+}
+
+void InitMembers(vector<bop>& members)
+{
+	ifstream infile;
+	infile.open(FILE_PATH);
+	if (!infile.is_open()) return;
+
+	string line;
+	while (getline(infile, line))
+	{
+		string buffer;
+		stringstream ss(line);
+		vector<string> bopInfo;
+
+		while (getline(ss, buffer, ','))
+		{
+			bopInfo.push_back(buffer);
+		}
+		if (bopInfo.size() != 4)
+		{
+			cout << "File format error";
+			return;
+		}
+		bop member;
+		member.fullname = bopInfo[0];
+		member.title = bopInfo[1];
+		member.bopname = bopInfo[2];
+		member.preference = stoi(bopInfo[3]);
+
+		members.push_back(member);
+	}
+	infile.close();
+}
+
+void AddMember(vector<bop>& members)
+{
+	string fullname, title, bopname;
+	int preference;
+
+	cout << "\n회원 이름: ";
+	getline(cin, fullname);
+
+	cout << "회원 직함: ";
+	getline(cin, title);
+
+	cout << "BOP 아이디: ";
+	getline(cin, bopname);
+
+	cout << "지정 방식 (0 = fullname, 1 = title, 2 = bopname): ";
+	cin >> preference;
+	getchar();
+
+	bop newMember;
+	newMember.fullname = fullname;
+	newMember.title = title;
+	newMember.bopname = bopname;
+	newMember.preference = preference;
+
+	members.push_back(newMember);
+
+	ofstream outfile(FILE_PATH, ios::app);
+	if (!outfile.is_open()) return;
+
+	outfile << fullname << ',' << title << ',' << bopname << ',' << preference << '\n';
+	outfile.close();
+}
+
+void PrintMenu()
+{
+	cout << "\n==== Benevolent Order of Programmers ====\n\n"
+		<< "a. 회원 등록                  b. 실명으로 열람" << '\n'
+		<< "c. 직함으로 열람              d. BOP 아이디로 열람" << '\n'
+		<< "e. 회원이 지정한 것으로 열람  q. 종료" << '\n'
+		<< "원하는 것을 선택하십시오: ";
+}
+
+void PrintMembers(vector<bop>& bopList, BopId id)
+{
+	cout << '\n';
+	for (const auto& member : bopList)
+	{
+		BopId idType = (id == BOP_PREFERENCE) ? BopId(member.preference) : id;
+
+		switch (idType)
+		{
+		case BOP_FULLNAME:
+			cout << member.fullname << '\n';
+			break;
+
+		case BOP_TITLE:
+			cout << member.title << '\n';
+			break;
+
+		case BOP_NAME:
+			cout << member.bopname << '\n';
+			break;
+		}
+	}
+}
