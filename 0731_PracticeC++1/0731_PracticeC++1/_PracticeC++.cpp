8f2052b2cb42f0e31341bdea00999c45baadb23c
catnip144 <skytower144@naver.com>
2024-07-31 17:50:17 +0900

    07/31

---
+++ 0731_PracticeC++1/0731_PracticeC++1/_PracticeC++.cpp
@@ -0,0 +1,119 @@
+﻿/*
+C++ 기초 플러스 5번, 6번
+*/
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <string>
+#include <vector>
+#include <stdio.h>
+
+using namespace std;
+
+void Important()
+{
+	char* words[50];
+
+	for (int i = 0; i < 5; i++)
+	{
+		words[i] = new char[100];
+		cin >> words[i];
+	}
+	for (int i = 0; i < 5; i++)
+	{
+		cout << words[i] << " ";
+		delete words[i];
+	}
+}
+
+
+template <typename T> T max5(vector<T>& nums)
+{
+	T maxNum = nums[0];
+	for (int i = 1; i < nums.size(); i++)
+	{
+		if (maxNum < nums[i])
+			maxNum = nums[i];
+	}
+	return maxNum;
+}
+
+template <typename T> T maxn(vector<T>& elements)
+{
+	int maxIdx = 0;
+	for (int i = 1; i < elements.size(); i++)
+	{
+		if (elements[maxIdx] < elements[i])
+			maxIdx = i;
+	}
+	return elements[maxIdx];
+}
+
+//string& maxn(vector<string>& elements, int arrSize)
+//{
+//	int maxIdx = 0;
+//	for (int i = 1; i < arrSize; i++)
+//	{
+//		if (elements[maxIdx].size() < elements[i].size())
+//			maxIdx = i;
+//	}
+//	return elements[maxIdx];
+//}
+
+int main()
+{
+	//// 문제 5
+	//cout << "=== 문제 5 ===\n";
+
+	//vector<int> nums1(5, 0);
+	//cout << "5개의 숫자 입력 (int) : ";
+
+	//for (int i = 0; i < nums1.size(); i++)
+	//	cin >> nums1[i];
+
+	//int maxNum1 = max5(nums1);
+	//cout << "int형 가장 큰 숫자 : " << maxNum1 << endl;
+
+	//vector<double> nums2(5, 0);
+	//cout << "5개의 숫자 입력 (double) : ";
+
+	//for (int i = 0; i < nums2.size(); i++)
+	//	cin >> nums2[i];
+
+	//double maxNum2 = max5(nums2);
+	//cout << "double형 가장 큰 숫자 : " << maxNum2 << endl;
+
+
+	////// 문제 6
+	cout << "\n=== 문제 6 ===\n";
+
+	vector<int> nums3(6, 0);
+	cout << "6개의 숫자 입력 (int) : ";
+
+	for (int i = 0; i < nums3.size(); i++)
+		cin >> nums3[i];
+
+	int maxNum3 = maxn(nums3);
+	cout << "int형 가장 큰 숫자 : " << maxNum3 << endl;
+
+	vector<double> nums4(4, 0);
+	cout << "4개의 숫자 입력 (double) : ";
+
+	for (int i = 0; i < nums4.size(); i++)
+		cin >> nums4[i];
+
+	double maxNum4 = maxn(nums4);
+	cout << "double형 가장 큰 숫자 : " << maxNum4 << endl;
+
+	vector<string> words(5);
+	cout << "5개의 단어 입력 : ";
+
+	for (int i = 0; i < words.size(); i++)
+		cin >> words[i];
+
+	string maxLengthString = maxn(words, words.size());
+	cout << "가장 긴 문자열 : " << maxLengthString << endl;
+	
+
+	return 0;
+}
+
---
+++ 0731_PracticeC++2/0731_PracticeC++2/_PracticeC++.cpp
@@ -0,0 +1,135 @@
+﻿/*
+	자료구조, 알고리즘 122p Q4
+*/
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <string>
+#include <vector>
+#include <time.h>
+
+using namespace std;
+
+int BinarySearch(vector<int>& nums, int target);
+int SentinelLinearSearch(vector<int>& nums, int target);
+void PrintGuideline(vector<int>& nums, int left, int right, int mid, bool isHeadLine = false);
+
+int main()
+{
+	int n;
+	cout << "배열 크기 : ";
+	cin >> n;
+
+	vector<int> nums(n + 1);
+	clock_t startTime, endTime;
+
+	for (int i = 0; i < n; i++)
+		nums[i] = i;
+
+	int targetIndex;
+	float executionTime;
+	
+	//PrintGuideline(nums, 0, 0, 0, true);
+
+	executionTime = 0;
+	for (int target = 0; target < n; target++)
+	{
+		startTime = clock();
+		targetIndex = BinarySearch(nums, target);
+		endTime = clock();
+
+		executionTime += (float)(endTime - startTime) / CLOCKS_PER_SEC;
+	}
+	cout << "0 ~ n-1 까지 각 숫자의 이진 탐색 실행 시간 합 : " << executionTime << endl << endl;
+
+	executionTime = 0;
+	for (int target = 0; target < n; target++)
+	{
+		startTime = clock();
+		targetIndex = SentinelLinearSearch(nums, target);
+		endTime = clock();
+
+		executionTime += (float)(endTime - startTime) / CLOCKS_PER_SEC;
+	}
+	cout << "0 ~ n-1 까지 각 숫자의 보초법 실행 시간 합 : " << executionTime << endl << endl;
+
+	return 0;
+}
+
+int BinarySearch(vector<int>& nums, int target)
+{
+	int left = 0;
+	int right = nums.size() - 1;
+	int mid;
+
+	while (left <= right)
+	{
+		mid = (left + right) / 2;
+		//PrintGuideline(nums, left, right, mid);
+
+		if (nums[mid] == target)
+			return mid;
+		
+		if (nums[mid] < target)
+			left = mid + 1;
+		else
+			right = mid - 1;
+	}
+	return -1;
+}
+
+int SentinelLinearSearch(vector<int>& nums, int target)
+{
+	int n = nums.size() - 1;
+	nums[n] = target;
+
+	for (int i = 0; ; i++)
+	{
+		if (nums[i] == target)
+		{
+			if (i == n)
+				return -1;
+			else
+				return i;
+		}
+	}
+}
+
+
+void PrintGuideline(vector<int>& nums, int left, int right, int mid, bool isHeadLine)
+{
+	if (isHeadLine)
+	{
+		cout << "   |";
+		for (int i = 0; i < nums.size(); i++)
+			cout << "   " << i;
+		cout << "\n---+----";
+
+		for (int i = 1; i < nums.size(); i++)
+			cout << "----";
+		cout << endl;
+		return;
+	}
+	cout << "   |";
+
+	int k = 2;
+	if (left == right) k = 3;
+
+	for (int i = 0; i < (left + 1) * 4 - k; i++)
+		cout << " ";
+	cout << "<-";
+
+	for (int i = 0; i < (mid - left) * 4 - 1; i++)
+		cout << " ";
+	cout << "+";
+
+	for (int i = 0; i < (right - mid) * 4 - 1; i++)
+		cout << " ";
+	cout << "->";
+
+	cout << endl << "  " << mid << "|";
+
+	for (int i = 0; i < nums.size(); i++)
+		cout << "   " << nums[i];
+
+	cout << endl << "   |" << endl;
+}
