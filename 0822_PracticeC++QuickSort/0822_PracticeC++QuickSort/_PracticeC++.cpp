fa4c106d229e514bb023de1cd2f0bc037cebd159
catnip144 <skytower144@naver.com>
2024-08-22 17:47:55 +0900

    08/22

---
+++ 0822_PracticeC++QuickSort/0822_PracticeC++QuickSort/_PracticeC++.cpp
@@ -0,0 +1,74 @@
+﻿/*
+	Quick Sort를 구현
+	비교횟수, 교환 횟수, 이전 알고리즘 비교
+	속도 측정 .. 
+*/
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <string>
+#include <vector>
+#include <time.h>
+#include <algorithm>
+
+using namespace std;
+
+void QuickSort(vector<int>& nums, int start, int end)
+{
+	if (end <= start)
+		return;
+
+	int pivot = start;
+	int pl = start + 1;
+	int pr = end;
+
+	// 엇갈리기 전까지 반복
+	while (pl <= pr)
+	{
+		while (pl < end && nums[pl] <= nums[pivot])
+			++pl;
+
+		while (pr > start && nums[pr] >= nums[pivot])
+			--pr;
+
+		// 같은 위치 혹은 엇갈렸을 때
+		if (pl >= pr)
+			break;
+
+		int temp = nums[pl];
+		nums[pl] = nums[pr];
+		nums[pr] = temp;
+	}
+	int temp = nums[pivot];
+	nums[pivot] = nums[pr];
+	nums[pr] = temp;
+
+	QuickSort(nums, start, pr - 1);
+	QuickSort(nums, pr + 1, end);
+}
+
+int main()
+{
+	cout << "배열 크기 입력: ";
+	int n;
+	cin >> n;
+
+	vector<int> nums;
+	for (int i = 1; i <= n; i++)
+		nums.push_back(i);
+
+	srand(time(NULL));
+	random_shuffle(nums.begin(), nums.end());
+
+	for (auto num : nums)
+		cout << num << " ";
+	cout << endl;
+	
+	QuickSort(nums, 0, n - 1);
+
+	for (auto num : nums)
+		cout << num << " ";
+	cout << endl;
+
+	return 0;
+}
+
---
+++ 0822_wp_1/0822_wp_1/problem.cpp
@@ -0,0 +1,25 @@
+/*
+Q1.	격자 그리기
+	DrawGrid(...) 함수 구현하라
+	격차 위치, 격자 Width, Height
+	격자 개수 또는 격자 간격을 인자로 한다
+
+	- void DrawGrid(HDC hdc, POINT center, int Width, int Height, int Count = 0)
+
+Q2.	원 그리기
+	DrawCircle(...) 함수를 구현하라.
+	원의 중점좌표, 반지름을 인자로 한다.
+
+	- void DrawCircle(HDC hdc, POINT center, int Radius)
+
+
+Q3. 해바라기 그리기 함수를 구현하라.
+	원을 그리기 위한 기본 정보에 외각에 그려질 원의 개수를 입력받아
+	해바라기 형식으로 그려지도록 한다.
+
+	- void DrawSunflower(HDC hdc, ....)
+
+
+
+
+*/
