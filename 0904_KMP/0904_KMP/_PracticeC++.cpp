2c6a380f3183266840e416ce527f84d4f668a058
skytower144 <127186795+catnip144@users.noreply.github.com>
2024-09-04 22:59:27 +0900

    Update _PracticeC++.cpp

--- 0904_KMP/0904_KMP/_PracticeC++.cpp
+++ 0904_KMP/0904_KMP/_PracticeC++.cpp
@@ -1,22 +1,64 @@
-﻿/*
-
-*/
-#define _CRT_SECURE_NO_WARNINGS
-#include <iostream>
+﻿#include <iostream>
 #include <string>
 #include <vector>
 
 using namespace std;
 
-void CreateChart(vector<int>& chart, string& text, string& pattern)
+void PrintProgress(string& text, string& pattern, int i, int j, bool isMatch)
 {
-	for (int i = 0; i < text.size(); i++)
-	{
-		for (int j = 0; j < text.size(); j++)
-		{
+	char c = isMatch ? '+' : '|';
+	cout << text << endl;
 
+	for (int k = 0; k < i; k++)
+		cout << " ";
+	cout << c << endl;
+
+	for (int k = 0; k < i - j; k++)
+		cout << " ";
+	cout << pattern << endl << endl;
+}
+
+void CreateTable(vector<int>& table, string& pattern)
+{
+	int i = 0;
+	while (i < pattern.size())
+	{
+		for (int j = 0; j <= i; j++)
+		{
+			++i;
+			if (pattern[i] == pattern[j])
+				table[i] = table[i - 1] + 1;
+			else
+				break;
 		}
 	}
+}
+
+int KMP(vector<int>& table, string& pattern, string& text)
+{
+	int i = 0, j = 0;
+	while (i < text.size() && j < pattern.size())
+	{
+		if (text[i] == pattern[j])
+		{
+			PrintProgress(text, pattern, i, j, true);
+			++j;
+			++i;
+		}
+		else if (j == 0)
+		{
+			PrintProgress(text, pattern, i, j, false);
+			++i;
+		}
+		else
+		{
+			PrintProgress(text, pattern, i, j, false);
+			j = table[j - 1];
+		}
+	}
+	if (j == pattern.size())
+		return i - j;
+	return -1;
 }
 
 int main()
@@ -29,10 +71,20 @@ int main()
 	cout << "패턴 입력:";
 	cin >> pattern;
 
-	vector<int> chart;
-	CreateChart(chart, text, pattern);
+	vector<int> table(pattern.size(), 0);
+	CreateTable(table, pattern);
 
+	cout << "=== Table ===" << endl;
+	for (int& num : table)
+		cout << num << " ";
+	cout << endl << endl;
 
+	int matchIdx = KMP(table, pattern, text);
+
+	if (matchIdx != -1)
+		cout << matchIdx + 1 << "번째에 일치합니다.\n";
+	else
+		cout << "패턴이 존재하지 않습니다.\n";
 
 	return 0;
 }
