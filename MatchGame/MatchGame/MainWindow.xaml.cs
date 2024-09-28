58b182813e4b6fad7bf040b93ccc9c6f47c7c2b1
catnip144 <skytower144@naver.com>
2024-09-28 13:12:04 +0900

    Find all of the matching food

---
+++ MatchGame/MatchGame/MainWindow.xaml.cs
@@ -0,0 +1,100 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace MatchGame
+{
+    using System.Windows.Threading;
+
+    public partial class MainWindow : Window
+    {
+        DispatcherTimer timer = new DispatcherTimer();
+        int tenthsOfSecondsElapsed;
+        int matchesFound;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            timer.Interval = TimeSpan.FromSeconds(.1);
+            timer.Tick += Timer_Tick;
+            SetUpGame();
+        }
+
+        private void Timer_Tick(object? sender, EventArgs e)
+        {
+            tenthsOfSecondsElapsed++;
+            TimeTextBlock.Text = (tenthsOfSecondsElapsed / 10F).ToString("0.0s");
+            if (matchesFound == 8)
+            {
+                timer.Stop();
+                TimeTextBlock.Text = TimeTextBlock.Text + " - Play again?";
+            }
+        }
+
+        private void SetUpGame()
+        {
+            List<string> foodEmoji = new List<string>()
+            {
+                "🍕", "🍕",
+                "🍔", "🍔",
+                "🍟", "🍟",
+                "🌭", "🌭",
+                "🍘", "🍘",
+                "🍙", "🍙",
+                "🍚", "🍚",
+                "🍛", "🍛"
+            };
+            Random random = new Random();
+            foreach (TextBlock textBlock in mainGrid.Children.OfType<TextBlock>())
+            {
+                if (textBlock.Name != "TimeTextBlock")
+                {
+                    textBlock.Visibility = Visibility.Visible;
+                    int index = random.Next(foodEmoji.Count);
+                    string nextemoji = foodEmoji[index];
+                    textBlock.Text = nextemoji;
+                    foodEmoji.RemoveAt(index);
+                }
+            }
+            timer.Start();
+            tenthsOfSecondsElapsed = 0;
+            matchesFound = 0;
+        }
+
+        TextBlock lastTextBlockClicked;
+        bool findingMatch = false;
+
+        private void TextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            TextBlock textBlock = sender as TextBlock;
+            if (textBlock == null) return;
+
+            if (findingMatch == false)
+            {
+                textBlock.Visibility = Visibility.Hidden;
+                lastTextBlockClicked = textBlock;
+                findingMatch = true;
+            }
+            else if (textBlock.Text == lastTextBlockClicked.Text)
+            {
+                matchesFound++;
+                textBlock.Visibility = Visibility.Hidden;
+                findingMatch = false;
+            }
+            else
+            {
+                lastTextBlockClicked.Visibility = Visibility.Visible;
+                findingMatch = false;
+            }
+        }
+
+        private void TimeTextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (matchesFound == 8)
+            {
+                SetUpGame();
+            }
+        }
+    }
+}
