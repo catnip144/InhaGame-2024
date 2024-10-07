27ed6616fdb240c80e43314c36da1651d6127147
catnip144 <skytower144@naver.com>
2024-10-07 10:34:35 +0900

    winapi 3d

---
+++ WinAPI3D/WinAPI3D/cMainGame.cpp
@@ -0,0 +1,51 @@
+#pragma once
+#include "cMainGame.h"
+
+cMainGame::cMainGame()
+{
+}
+
+cMainGame::~cMainGame()
+{
+}
+
+void cMainGame::Update_Rotation()
+{
+}
+
+void cMainGame::Update_Move()
+{
+}
+
+void cMainGame::Update_Scale()
+{
+}
+
+void cMainGame::Setup()
+{
+}
+
+void cMainGame::Update()
+{
+}
+
+void cMainGame::Render(HDC hdc)
+{
+}
+
+void cMainGame::WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+}
+
+bool cMainGame::IsBackFace(cVector3& v1, cVector3& v2, cVector3& v3)
+{
+	return false;
+}
+
+void cMainGame::SetGrid()
+{
+}
+
+void cMainGame::DrawGrid()
+{
+}
